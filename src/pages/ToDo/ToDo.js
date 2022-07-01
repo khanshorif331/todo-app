97852d12e45bcc56afb80d311e6bbd6e37549b5e
khanshorif331 <khanshorif331@gmail.com>
2022-07-01 19:57:43 +0600

    todo data sending to database system implemented

--- src/pages/ToDo/ToDo.js
+++ src/pages/ToDo/ToDo.js
@@ -1,15 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import auth from '../../firebase.init'
 
 const ToDo = () => {
+	const [user] = useAuthState(auth)
+
 	const handleSubmit = e => {
 		e.preventDefault()
-		console.log('Form submitted')
+		const todo = e.target.todo.value
+		const item = {
+			todo: todo,
+			user: user?.email,
+			status: 'pending',
+		}
+		console.log(item)
+		fetch('http://localhost:5000/todo', {
+			method: 'POST',
+			headers: {
+				'content-type': 'application/json',
+			},
+			body: JSON.stringify(item),
+		})
+			.then(res => res.json())
+			.then(data => console.log(data))
 	}
+	// console.log(todo, 'after', user)
 	return (
 		<div>
 			<form onSubmit={handleSubmit}>
 				<input
 					type='text'
+					name='todo'
 					placeholder='Type here'
 					class='input input-bordered input-primary w-full max-w-xs'
 				/>
